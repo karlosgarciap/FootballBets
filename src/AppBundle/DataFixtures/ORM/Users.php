2996db8520b81acd29230be224f7fe225aaf7c59
Jordi Rejas <jordi@rejas.eu>
2018-07-14 18:23:17 +0200

    - FosUserBundle - DoctrineFixturesBundle - User entity and fixtures

---
+++ src/AppBundle/DataFixtures/ORM/Users.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class Users extends Fixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+	/**
+	 * @var ContainerInterface
+	 */
+	private $container;
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function setContainer(ContainerInterface $container = null)
+	{
+		$this->container = $container;
+	}
+	
+	public function load(ObjectManager $manager)
+	{
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        $phone = 666666666;
+
+        for ($i = 1; $i <= 50; $i ++){
+
+            /** @var User $user */
+            $user = $userManager->createUser();
+            $user->setName('User ' . $i)
+                ->setSurname('Surname ' . $i)
+                ->setPhone($phone++)
+                ->setBalance(rand(5, 50))
+                ->setEmail('user' . $i . '@footballbets.com')
+                ->setUsername('user' . $i)
+                ->setEnabled(1)
+                ->setPlainPassword('user' . $i);
+            $userManager->updateUser($user);
+        }
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getOrder()
+	{
+		return 1;
+	}
+}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $surname;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $phone;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $balance;
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     *
+     * @return User
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param integer $phone
+     *
+     * @return User
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return integer
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set balance
+     *
+     * @param integer $balance
+     *
+     * @return User
+     */
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    /**
+     * Get balance
+     *
+     * @return integer
+     */
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+}
