7051a588074d63107ed02b8519169d6c61a2ba4c
Jordi Rejas <jordi@rejas.eu>
2018-07-19 18:25:47 +0200

    Refactor matches fixtures

--- src/AppBundle/DataFixtures/ORM/Matches.php
+++ src/AppBundle/DataFixtures/ORM/Matches.php
@@ -1,32 +1,71 @@
 <?php
+
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\Match;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Matches extends Fixture implements OrderedFixtureInterface
+class Matches extends Fixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
     /**
-     * @var Match
+     * @var ContainerInterface
      */
-    private $match;
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
 
     public function load(ObjectManager $manager)
     {
-        $teams = $manager->getRepository('AppBundle:Team')->findAll();
-        foreach ($teams as $index => $team) {
-            if ($index%2 == 0) {
-                $this->match = new Match();
-                $this->match->setTeam1($team);
-            } else {
-                $this->match->setTeam2($team)
-                ->setDate(new \DateTime());
-                $manager->persist($this->match);
+        $pathToFixturesFiles =  $this->container->get('kernel')->getRootDir() .
+            '/../src/AppBundle/DataFixtures/ORM/files/matches';
+
+        $resource = opendir($pathToFixturesFiles);
+
+        $teams = [];
+
+        while (false !== ($file = readdir($resource))) {
+            if (!in_array($file, ['.', '..'])) {
+                $json = json_decode(file_get_contents($pathToFixturesFiles . '/' . $file));
+                foreach ($json->matches as $matchData) {
+
+                    if(!isset($teams[$matchData->homeTeam->id])){
+                        $teams[$matchData->homeTeam->id] =
+                            $manager->getRepository('AppBundle:Team')
+                                ->findOneBy(['name' => $matchData->homeTeam->name]);
+                    }
+
+                    if(!isset($teams[$matchData->awayTeam->id])){
+                        $teams[$matchData->awayTeam->id] =
+                            $manager->getRepository('AppBundle:Team')
+                                ->findOneBy(['name' => $matchData->awayTeam->name]);
+                    }
+
+                    $match = new Match();
+                    $match->setTeam1($teams[$matchData->homeTeam->id])
+                        ->setTeam2($teams[$matchData->awayTeam->id])
+                        ->setGoals1($matchData->score->fullTime->homeTeam != null ?
+                            $matchData->score->fullTime->homeTeam : rand(0, 7))
+                        ->setGoals2($matchData->score->fullTime->awayTeam != null ?
+                            $matchData->score->fullTime->awayTeam : rand(0, 7))
+                        ->setDate(\DateTime::createFromFormat(\DateTime::W3C, $matchData->utcDate))
+                    ;
+
+                    $manager->persist($match);
+                }
             }
         }
 
+        closedir($resource);
         $manager->flush();
     }
 
